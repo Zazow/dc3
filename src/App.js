646183a19a3af2d76320e8b1935c6aacfb4f3234
ziyad <ziyadkag@gmail.com>
2019-12-06 10:11:15 -0600

    changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
-import data from "./all-nodes.json"
+import data from "./csvjson.json"
 import './App.css';
 import TextField from '@material-ui/core/TextField';
 import Autocomplete from '@material-ui/lab/Autocomplete';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { useTheme, makeStyles } from '@material-ui/core/styles';
+import { ResponsiveTreeMap } from '@nivo/treemap'
 
+function compileTree(data, id) {
+  let children = data[id].children;
+  let root = {
+    name: data[id].name,
+    loc: data[id].productCount,
+    children: []
+  };
+  for (let child of children) {
+    root.children.push(compileTree(data, child));
+  }
+
+  return root;
+}
 
 let categories = []
 
@@ -12,14 +28,9 @@ for (let node of data) {
     categories.push(node.name)
 }
 
-console.log(categories)
 
-let root = {};
-
-
-
-console.log(data);
-
+let root = compileTree(data, 0);
+console.log(root);
 
 function App() {
   return (
@@ -36,20 +47,25 @@ function App() {
       )}
       />
 
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello dc3
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ width: window.innerWidth,  height: window.innerHeight}}>
+      <ResponsiveTreeMap
+        root={root}
+        identity="name"
+        value="loc"
+        innerPadding={3}
+        outerPadding={3}
+        margin={{ top: 10, right: 10, bottom: 10, left: 10 }}
+        label="name"
+        labelFormat=".0s"
+        labelSkipSize={12}
+        labelTextColor={{ from: 'color', modifiers: [ [ 'darker', 1.2 ] ] }}
+        colors={{ scheme: 'nivo' }}
+        borderColor={{ from: 'color', modifiers: [ [ 'darker', 0.3 ] ] }}
+        animate={true}
+        motionStiffness={90}
+        motionDamping={11}
+      />
+      </div>
     </div>
   );
 }
