bd08cce6de9b79dbb9e1cdfe548c3d4e69145a15
Ziyad AlGhunaim <ziyadkag@gmail.com>
2019-12-13 16:12:10 -0800

    Updates

---
+++ precache-manifest.a430d61bdc220aed6d21398a9b3355c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b707edcc132edf0e912d74cbdb74caee",
+    "url": "/dc3/index.html"
+  },
+  {
+    "revision": "9c09076de0cfbaa972a3",
+    "url": "/dc3/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "5e957c1afa8459414086",
+    "url": "/dc3/static/js/2.1626d14e.chunk.js"
+  },
+  {
+    "revision": "9c09076de0cfbaa972a3",
+    "url": "/dc3/static/js/main.61510e12.chunk.js"
+  },
+  {
+    "revision": "32ab858c1bea5c37419c",
+    "url": "/dc3/static/js/runtime-main.0e453fc9.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/dc3/static/media/logo.25bf045c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dc3/precache-manifest.e3f5bc4612da38dd315a0b4cd79301c1.js"
+  "/dc3/precache-manifest.a430d61bdc220aed6d21398a9b3355c0.js"
 );
 
 self.addEventListener('message', (event) => {
